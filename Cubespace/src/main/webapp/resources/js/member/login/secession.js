c71463ce45ab933b329c477209e493fdad6eab5c
dawonleeee <ekdnjs210@gmail.com>
2022-12-20 14:16:15 +0900

    내정보수정, 비밀번호변경 html

---
+++ Cubespace/src/main/webapp/resources/js/member/login/secession.js
@@ -0,0 +1,28 @@
+function varifySecession(){
+
+    const memberEmail = document.getElementById("memberEmail");
+    const memberName = document.getElementById("memberName");
+    const memberPw = document.getElementById("memberPw");
+
+
+    if(memberEmail.value.trim().length == 0){
+        alert("아이디(이메일)를 입력해주세요");
+        return false;
+    }
+    if(memberName.value.trim().length == 0){
+        alert("이름을 입력해주세요");
+        return false;
+    }
+    if(memberPw.value.trim().length == 0){
+        alert("비밀번호를 입력해주세요");
+        return false;
+    }
+
+    if(confirm("정말 탈퇴하겠습니까?")){
+        return true;
+
+    } else {
+        alert("탈퇴 취소");
+        return false;
+    }
+}

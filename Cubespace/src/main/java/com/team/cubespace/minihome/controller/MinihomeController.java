d758dfe585292b33c46b6511ed1135049f67e26d
Tonic Kim <79572904+dudgus197493@users.noreply.github.com>
2022-12-20 16:13:26 +0900

    12/20 미니홈피 프레임 임시 커밋
    미니홈피 프레임 임시 커밋

--- Cubespace/src/main/java/com/team/cubespace/minihome/controller/MinihomeController.java
+++ Cubespace/src/main/java/com/team/cubespace/minihome/controller/MinihomeController.java
@@ -3,10 +3,18 @@ package com.team.cubespace.minihome.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/** 미니홈피 프레임 기능 관련 컨트롤러
+ * @author Tonic
+ *
+ */
 @Controller
 public class MinihomeController {
 	
-	@GetMapping("/minihome")
+	
+	/** 미니홈피 열기
+	 * @return minihome/minihome-frame 포워드
+	 */
+	@GetMapping("/minihome")	// @pathVariable memberNo 필요
 	public String minihome() {
 		return "minihome/minihome-frame";
 	}

c27c80cec5d8e668e605aa5449471de3fdb1b3f1
muuung <caninespaw@gmail.com>
2022-12-27 09:20:42 +0900

    2022-12-27

--- Cubespace/src/main/webapp/resources/js/minihome/minihome-home.js
+++ Cubespace/src/main/webapp/resources/js/minihome/minihome-home.js
@@ -1,5 +1,22 @@
-// 처음으로 스크롤
-$("#topBtn").click(function(){
+// 메시지 더보기
+function more(){
+    $(".friend-message:hidden").slice(0, 5).css("display", "flex");
+    if($(".friend-message:hidden").length == 0){
+        $("#moreBtn").css("display", "none");
+    }
+}
+
+$(() => {
+    more();
+    
+    $("#moreBtn").click(e => {
+        e.preventDefault();
+        more();
+    });
+})
+
+// 상단으로 가기
+$("#topBtn").click(() => {
 	$("#homeArea").animate({scrollTop : 0}, 400);
 	return false;
 });
---
+++ Cubespace/src/main/webapp/resources/js/minihome/miniroom/miniroom.js
@@ -0,0 +1,52 @@
+/* 이미 배치되어 있던 소품 좌표 불러오기 */
+
+// 미니룸 컨테이너 좌표
+const miniroomContainer = document.querySelector(".miniroom-container");
+const miniroomX = window.pageYOffset + miniroomContainer.getBoundingClientRect().left;
+const miniroomY = window.pageYOffset + miniroomContainer.getBoundingClientRect().top;
+
+const tileContainer = document.querySelector(".tile-container");
+
+(() => {
+    let i = 7;
+
+    // 해당 요소 아이디로 가져오기
+    let tileId = "tile" + i;
+    const tile = document.getElementById(tileId);
+    tile.classList.add("already");
+    tile.classList.remove("empty");
+
+    let tileLocationId = "tileLocation" + i;
+    let tileLocation = document.getElementById(tileLocationId);
+
+    // 타일의 좌표
+    const tileX = window.pageYOffset + tileLocation.getBoundingClientRect().left;
+    const tileY = window.pageYOffset + tileLocation.getBoundingClientRect().top;
+
+    // 소품 추가
+    const prop = document.createElement("div");
+    prop.classList.add("prop");
+    
+    // 소품 이미지 추가
+    const propImg = document.createElement("img");
+    propImg.src = "../../../images/ribbit.png";
+
+    prop.appendChild(propImg);
+    miniroomContainer.appendChild(prop);
+
+    // 소품의 좌표
+    let propX = tileX-miniroomX-prop.offsetWidth/2;
+    let propY = tileY-miniroomY-prop.offsetHeight/1.2;
+
+    prop.style.left = propX + "px";
+    prop.style.top = propY + "px";
+    // tileContainer.style.display = "none";
+
+    prop.addEventListener("click", () => {
+        const dropdown = document.querySelector(".dropdown");
+        dropdown.style.height = "50px";
+        dropdown.style.border = "1px solid black";
+        dropdown.style.left = propX + "px";
+        dropdown.style.top = propY - 50 + "px";
+    })
+})();
